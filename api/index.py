3c32f5132c374e9a5bdce49a9e6243fbe2161b6a
Yenuli <yenuli.cloud99x@gmail.com>
2026-07-03 22:54:15 +0530

    fix: css Styling issue

--- api/index.py
+++ api/index.py
@@ -1,9 +1,13 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, ROOT_DIR)
 
 from app import app
+
+app.template_folder = os.path.join(ROOT_DIR, 'templates')
+app.static_folder = os.path.join(ROOT_DIR, 'static')
 
 if __name__ == '__main__':
     app.run()
